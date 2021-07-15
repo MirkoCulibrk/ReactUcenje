d7de85bdb32fdba790a49bad42436e99a0940e92
Mirko Culibrk <m.culibrk@vega.local>
2021-07-15 11:14:45 +0200

    first commit

--- server1.js
+++ server1.js
@@ -1,23 +1,42 @@
+require('dotenv').config()
 const express=require('express');
+const mongoose=require('mongoose');
 const session=require('express-session');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const port = process.env.port || 5000;
+const router=require('./router/user');
 const app=express();
-const port = process.env.PORT || 5000; //Line
-app.use(cors());
+mongoose.connect(process.env.MONGOURL,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connection.on('connected',()=>{
+    console.log('conected to db');
+})
+const corsOptions={
+    "origin": 'http://localhost:3000',
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "credentials": true
+  }
+app.use(cors(corsOptions));
+const SESSION_NAME='sid';
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
+
 app.use(session({
-    name:'sid',
+    name:SESSION_NAME,
     resave:false,
     saveUninitialized:false,
-    secret:'ssh!quiet,itdas',
+    secret:'JRZ5LUIaTbBaHXWp',
     cookie:{
-        maxAge:60*60*1000,
-        sameSite:true,
-
+        maxAge:60*60*1000*10,
+        sameSite:false,
+        httpOnly:true
     }
-}))
-app.use('/login',(req,res)=>{
-    res.send({
-        token:'test'
-    })
-})
+}));
+
+app.use('/',router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
